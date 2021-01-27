098f6c8e469ec9516a63179db779ffbd05007135
chukkydave <chukkydave@gmail.com>
2021-01-27 17:17:05 +0100

    added the starter seedColors js file and creted the pallete component

---
+++ src/pallete.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Pallete extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
+
+	render() {
+		return (
+			<div>
+				<h1>Pallete</h1>
+			</div>
+		);
+	}
+}
+
+export default Pallete;
